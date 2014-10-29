a027743a6365e41ea7d01586c09c6667288608fd
sunilpottumuttu <mailtop.sunil@gmail.com>
2014-10-29 07:24:48 +0530

    Added CSharp REPL

---
+++ CSharpREPL/CONSTANTS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpREPL
+{
+    public class CONSTANTS
+    {
+        public const string WINDOWTITLE = "CSharp REPL Based on Roslyn";
+        public const string BANNERMESSAGE = "CSharp REPL Based on Roslyn\n" ;
+        public const string PROMPT = "SharpREPL>";
+
+    }
+}
---
+++ CSharpREPL/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using Roslyn.Compilers;
+using Roslyn.Scripting.CSharp;
+using Roslyn.Scripting;
+
+
+namespace CSharpREPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = CONSTANTS.WINDOWTITLE;
+            Console.SetWindowPosition(0, 0);
+            Console.WindowHeight = Console.LargestWindowHeight/2;
+            Console.WindowWidth = Console.LargestWindowWidth/2;
+            Console.WriteLine(CONSTANTS.BANNERMESSAGE);
+
+            var engine = new ScriptEngine();
+            var session = engine.CreateSession();
+
+            #region Add Assemblies SearchPaths
+            string[] searchPaths = File.ReadAllLines("SearchPaths.txt");
+            for (int i = 0; i < searchPaths.Length; i++)
+            {
+                if (searchPaths[i].Trim() == "")
+                {
+                    //Ignore empty lines
+                }
+                else
+                {
+                    try
+                    {
+                        session.SetReferenceSearchPaths(searchPaths[i]);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+            #endregion
+
+            #region Reference Assemblies 
+            string[] references = File.ReadAllLines("References.txt");
+            for (int i = 0; i < references.Length; i++)
+            {
+                if (references[i].Trim() == "")
+                {
+                    //Ignore empty lines
+                }
+                else
+                {
+                    try
+                    {
+                        session.AddReference(references[i]);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+            #endregion
+
+            #region Import Namespaces
+            string[] namespaces = File.ReadAllLines("Namespaces.txt");
+            for (int i = 0; i < namespaces.Length; i++)
+            {
+                if (namespaces[i].Trim() == "")
+                {
+                    //Ignore empty lines
+                }
+                else
+                {
+                    try
+                    {
+                        session.ImportNamespace(namespaces[i]);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+            #endregion
+            
+            for(;;)
+            {
+                try
+                {
+                    Console.Write(CONSTANTS.PROMPT);
+                    string code = Console.ReadLine();
+                    
+                    session.Execute(code);
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+
+
+        }
+    }
+}
